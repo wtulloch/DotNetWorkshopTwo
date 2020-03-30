9bd362404d6eaf2ebd4422f411c5e8cab8f105d8
William Tulloch <william.tulloch@readify.net>
2020-03-30 21:44:28 +1100

    added placeholder unit test

---
+++ UserServicesTestFixtures/UserManagementTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+using NSubstitute;
+namespace UserServicesTestFixtures
+{
+    public class UserManagementTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
